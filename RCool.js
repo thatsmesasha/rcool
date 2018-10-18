4979a5ae473926327f2655830f3d8ea149c6ff8c
Oleksandra Fedorova <emoilenne@gmail.com>
2018-10-17 19:26:13 -0700

    change network interface from eth0 to wlan0

--- RCool.js
+++ RCool.js
@@ -4,13 +4,13 @@ const controlServer = require('./RCool_control_server')
 const os = require('os')
 
 try {
+  const url = os.networkInterfaces().wlan0[0].address
+
   cameraServer.run()
   controlServer.run()
 
   const app = express()
   const port = 4200
-
-  const url = os.networkInterfaces().eth0.address
 
   app.set('view engine', 'ejs')
   app.get('/', (req, res) => res.render(`${__dirname}/index`, { url }))
