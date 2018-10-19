957fa7889160c73737004105a596de2f410300ce
Oleksandra Fedorova <emoilenne@gmail.com>
2018-10-18 17:54:27 -0700

    fix driving back issue

--- RCool_drive.py
+++ RCool_drive.py
@@ -8,23 +8,41 @@ from threading import Timer
 steering = Servo(PIN_A)
 speed = Servo(PIN_B)
 
+def drive_forward():
+  speed.value = 0.2
+  steering.mid()
+
+def drive_right():
+  speed.value = 0.2
+  steering.min()
+
+def drive_back():
+  speed.value = 0.2
+  steering.max()
+
+def drive_back():
+  speed.value = -0.3
+  steering.mid()
+
+def stop():
+  speed.value = 0
+  steering.mid()
+
 def drive(direction):
   if direction == 'forward':
     print('driving forward')
-    speed.value = 0.2
-    steering.mid()
+    drive_forward()
   elif direction == 'right':
     print('driving right')
-    speed.value = 0.2
-    steering.min()
+    drive_right()
   elif direction == 'left':
     print('driving left')
-    speed.value = 0.2
-    steering.max()
+    drive_left()
   elif direction == 'back':
     print('driving back')
-    speed.value = -0.3
-    steering.mid()
+    drive_back()
+    Timer(0.05, stop).start()
+    Timer(0.1, drive_back).start()
 
 def stop():
   print('stopping')
